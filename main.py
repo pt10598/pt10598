336b03a04bc0a77211a2a1c82d4de98889c67ad8
pt10598 <130521177+pt10598@users.noreply.github.com>
2023-07-12 15:29:57 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+app = Flask(__name__)
+iden=input('輸入車號：')
+chrome_options=Options()
+chrome_options.add_argument("--headless")
+driver=webdriver.Chrome(options=chrome_options)
+driver.get("https:pay.taipei/v2/CheckBill/Index/2")
+
+time.sleep(0.5)
+serc2 = driver.find_element("name", "parameter2")
+serc2.send_keys(iden)
+time.sleep(0.5)
+serc3=driver.find_element("xpath", '/html/body/article/main/form/div[3]/button')
+serc3.click()
+time.sleep(0.5)
+serc4=driver.find_element("xpath", '/html/body/article/main/form/section/div[4]/button')
+serc4.click()
+time.sleep(0.5)
+serc5=driver.find_element("xpath", '/html/body/article/main/form[1]/div[1]/div/span[2]')
+print(serc5.text)
+
+@app.route("/")
+def home():
+    return "<title>查詢</title>" \
+           f"<p>金額{serc5.text}元<p1>"\
+
+app.run(host="0.0.0.0",port=5000)
