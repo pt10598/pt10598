fc02192fcd1a27105b1bb66a547f08788c9b6a77
pt10598 <130521177+pt10598@users.noreply.github.com>
2023-07-28 16:01:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from flask import Flask, request, render_template, redirect, url_for
 from flask import Flask
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 app = Flask(__name__)
 
-@app.route('/')
-def formPage():
-    return "TEST"
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run()
